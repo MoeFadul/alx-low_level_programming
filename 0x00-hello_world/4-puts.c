2a8ef08d09d5eacbb8d0049ec5dda4fe876ae323
MoeFadul <fadulmoe@gmail.com>
2023-09-07 01:17:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,3 +4,7 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+/**
+ * main - display  "Programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program
+ */
