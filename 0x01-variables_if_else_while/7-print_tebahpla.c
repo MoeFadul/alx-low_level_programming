0543c2245374c987d88f8f3f9b6ee2269be42597
MoeFadul <fadulmoe@gmail.com>
2023-09-08 04:02:33 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,14 @@
 
 /**
  * main - display alphabet reverse and lowercase
- * Return: return 0 and exit program
+ * Return: return 0 and exit the program
  */
 int main(void)
 {
 	char l;
 
 	for (l = 'z'; l >= 'a'; l--)
-		putchar(l);
+			putchar(l);
 		putchar('\n');
 
 	return (0);
