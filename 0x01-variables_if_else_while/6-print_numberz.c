a40f64489410a7980d32240b9b761d4da13169ad
MoeFadul <fadulmoe@gmail.com>
2023-09-08 03:54:22 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - display numbers from 0 to 9 using putchar
+ * Return: return 0 and exit program.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
