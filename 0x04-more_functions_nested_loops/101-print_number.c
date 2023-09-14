db601887e421c8cc04691bf25239955169280754
MoeFadul <fadulmoe@gmail.com>
2023-09-14 04:42:14 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - check code
+ *
+ * @n: variables
+ *
+ * Return: Always
+ */
+
+void print_number(int n)
+{
+
+	unsigned int integer = n;
+
+	if  (n < 0)
+	{
+		integer = -n;
+		_putchar('-');
+	}
+
+	else
+	{
+		integer = n;
+	}
+
+	if (integer / 10)
+	{
+		print_number(integer / 10);
+	}
+
+	_putchar((integer % 10) + '0');
+}
