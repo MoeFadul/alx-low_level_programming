2349550571ee1e9cd9fd40c224644a8e428e049b
MoeFadul <fadulmoe@gmail.com>
2023-09-21 04:29:29 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int *p;
 a[2] = 1024;
 p = &n;
   /*
-   * main - write your line of code here...
+   * write your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
