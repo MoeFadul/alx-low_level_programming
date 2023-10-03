98aeba5aa6b5ed16bb9b26ec4f8436ee43b40b65
MoeFadul <fadulmoe@gmail.com>
2023-10-03 02:03:44 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -58,3 +58,69 @@ char **strtow(char *str)
 	words[wlen] = NULL;
 	return (words);
 }
+
+/**
+ * mywstr - function that converts words into strings.
+ * @words: the words to work on it.
+ * @str: the string to work on it.
+ * Return: nothing.
+ */
+
+void mywstr(char **words, char *str)
+{
+	int i, j, wstart, wrflag;
+
+	i = j = wrflag = 0;
+
+	while (str[i])
+
+	{
+		if (wrflag == 0 && str[i] != ' ')
+
+		{
+			wstart = i;
+			wrflag = 1;
+		}
+
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+
+		{
+			cmyword(words, str, wstart, i, j);
+			j++;
+			wrflag = 0;
+		}
+
+		i++;
+	}
+
+	if (wrflag == 1)
+	{
+		cmyword(words, str, wstart, i, j);
+	}
+
+}
+
+/**
+ * cmyword - creates a word from a string.
+ * @words: the words to insert to string.
+ * @str: the string to work on it.
+ * @start: the start position.
+ * @end: the stop position.
+ * @index: where to start inserting the new word.
+ * Return: nothing.
+ */
+
+void cmyword(char **words, char *str, int start, int end, int index)
+{
+	int i, j;
+
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; start < end; start++, j++)
+	{
+		words[index][j] = str[start];
+	}
+
+	words[index][j] = '\0';
+}
