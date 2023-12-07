3b14a9150e867e8045ac370f8b3ae2fb2c10900a
MoeFadul <fadulmoe@gmail.com>
2023-12-07 20:34:48 +0000

    102-result.py

---
+++ 0x17-doubly_linked_lists/102-result.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+rgest_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        product_str = str(product)
+        if product_str == product_str[::-1] and product > largest_palindrome:
+            largest_palindrome = product
+
+# Save the result in the file
+with open("102-result", "w") as file:
+    file.write(str(largest_palindrome))
